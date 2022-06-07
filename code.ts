0b92bbb389a13b5821f8899ccc8babfb8536e951
franni <franziska.haaf@teaminternet.com>
2022-06-07 17:49:24 +0200

    Some basic styling

--- code.ts
+++ code.ts
@@ -3,20 +3,18 @@
  * **/
 
 figma.showUI(__html__);
-
+figma.ui.resize(400, 300);
 
 figma.ui.onmessage = async msg => {
     if (msg.type === 'addTag') {
-        await figma.loadFontAsync({ family: "Inter", style: "Medium" })
-        await figma.loadFontAsync({ family: "Inter", style: "Bold" })
+        await figma.loadFontAsync({family: "Inter", style: "Medium"})
+        await figma.loadFontAsync({family: "Inter", style: "Bold"})
 
         var selectedStickies = figma.currentPage.selection;
 
         selectedStickies.forEach(sticky => {
             if (sticky.type === "STICKY") {
-                console.log(sticky.text.characters)
-                debugger;
-                sticky.text.characters +=  '\n\n' + msg.tagContent
+                sticky.text.characters ? (sticky.text.characters += '\n\n' + msg.tagContent) : (sticky.text.characters += msg.tagContent)
             }
         })
     }
