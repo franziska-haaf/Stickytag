49ad49a23632844066808e8dc33249ba0d696a20
franni <franziska.haaf@teaminternet.com>
2022-06-07 16:46:33 +0200

    Created primary functionality

--- code.ts
+++ code.ts
@@ -1,52 +1,25 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many shapes and connectors on the screen.
+/**
+ * This plugin adds a given Text and adds it to the end of the text of a sticky note
+ * **/
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    const numberOfShapes = msg.count;
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfShapes; i++) {
-      const shape = figma.createShapeWithText();
-      // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-      shape.shapeType = 'ROUNDED_RECTANGLE'
-      shape.x = i * (shape.width + 200);
-      shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(shape);
-      nodes.push(shape);
-    };
 
-    for (let i = 0; i < (numberOfShapes - 1); i++) {
-      const connector = figma.createConnector();
-      connector.strokeWeight = 8
+figma.ui.onmessage = async msg => {
+    if (msg.type === 'addTag') {
+        await figma.loadFontAsync({ family: "Inter", style: "Medium" })
+        await figma.loadFontAsync({ family: "Inter", style: "Bold" })
 
-      connector.connectorStart = {
-        endpointNodeId: nodes[i].id,
-        magnet: 'AUTO',
-      };
+        var selectedStickies = figma.currentPage.selection;
 
-      connector.connectorEnd = {
-        endpointNodeId: nodes[i+1].id,
-        magnet: 'AUTO',
-      };
-    };
+        selectedStickies.forEach(sticky => {
+            if (sticky.type === "STICKY") {
+                console.log(sticky.text.characters)
+                debugger;
+                sticky.text.characters +=  '\n\n' + msg.tagContent
+            }
+        })
+    }
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+    // todo figma.closePlugin();
 };
